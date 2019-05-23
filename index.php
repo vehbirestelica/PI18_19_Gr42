fd5069719eecfaea6d259b7a31f98a20b040044e
Vehbi Restelica <vehbi.res@gmail.com>
2019-05-24 01:25:49 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,341 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <style>
+    .tedrejtat
+    {
+      color: whitesmoke;
+
+    }
+    .rrjetet-sociale a
+        {
+            color: white;
+            text-decoration: none;
+        }
+        .rrjetet-sociale a:hover
+        {
+            color:chocolate;
+            text-decoration: underline;
+        }
+        .rrjetet-sociale a:active
+        {
+            color:darkgreen;
+        }
+ #signup
+    {
+        float: right;
+        margin-right: 27px;
+
+    }
+ #signup a 
+    {
+        text-decoration: none;
+        color: whitesmoke;    
+    }
+  #signup a:hover {
+  	color: red;
+  }
+  .sec
+        {
+            display: block;
+            margin-top: 29%;
+            margin-bottom: 0%; 
+            margin-right: 4%;
+            margin-left: 4%;     
+        }
+       .p11
+        {
+            align-content: left;
+        }
+        .p12
+        {
+            align-content: right;
+        }
+
+         .menu a
+        {
+
+text-align:center;
+font-family: Courier;
+transition: 0.3s;
+width:8.5%;
+padding:2px 5;
+text-decoration: none;
+color:whitesmoke;
+font-size:15px;
+        }
+        .menu2 a
+            {
+border-left: 1px solid gray;
+height: 18px;
+            }
+
+.quick-links {
+    width: 200px;
+}
+
+.quick-links a {
+
+    color: black;
+    display: block;
+    padding: 12px;
+    text-decoration: none;
+}
+
+.quick-links a:hover {
+    color: white;
+}
+
+.quick-links a:active {
+
+    color: orange;
+}
+    .fotot 
+        {
+            display: inline; 
+        }
+        .f1 
+        {
+            width: 33.3%;
+            height: 70vh;
+            margin-left: 8%;
+            margin-top: 0%;
+            position: absolute;
+            background-image: url(images/LM10.jpg);
+            background-repeat: no-repeat;  
+             
+        }
+        .f1 .t1 {
+            position: absolute;
+            width: 80%;
+            background-color: #0009;
+            transform:translate(-50%,-50%);
+            top: 70%;
+            left: 40%;
+            
+        }
+        .f1 .t1 p {
+            color:aliceblue;
+            font-size: 20.5px;
+            font-family: cursive;
+            text-align: center;
+            
+        }
+
+        .f2
+        {
+            width: 33.3%;
+            height: 70vh;
+            margin-left: 37.3%;
+            margin-top: 0%;
+            position: absolute;
+            background-image: url(images/GOT.jpg);
+            background-repeat: no-repeat;  
+           
+        }
+
+        .f2 .t2 {
+            position: absolute;
+            width: 80%;
+            background-color: #0009;
+            transform:translate(-50%,-50%);
+            top: 70%;
+            left: 40%;
+        }
+        
+        .f2 .t2 p 
+        {
+            color: aliceblue;
+            font-size: 20.5px;
+            font-family: cursive;
+            text-align: center;
+            
+        }
+        .f3 
+        {
+            width: 33.5%;
+            height: 70vh;
+            margin-left: 66.5%;
+            margin-top: 0%;
+            position: absolute;
+            background-image: url(images/SL.jpg);
+            background-repeat: no-repeat;  
+            
+            
+        }
+        .f3 .t3 
+        {
+            position: absolute;
+            width: 83.35%;
+            background-color: #0009;
+            transform:translate(-50%,-50%);
+            top: 70%;
+            left: 33%; 
+        }
+        .f3 .t3 p 
+        {
+            color: aliceblue;
+            font-size: 22.5px;
+            font-family: cursive;
+            text-align: center;
+
+        }    
+        a {
+        	text-decoration: none;
+        }
+        #linku {
+        	font-family: verdana;
+        	font-size: 16.3px;
+        }
+
+    </style>
+<link rel="stylesheet" href="styles2.css">
+<meta charset="utf-8">
+<title>Monumental </title>
+<h1 id="Home"><big>M</big>ONUMENTAL</h1>
+<h6>Online Magazine</h6>
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+<div id="signup">
+<a href="pages/signup.html"> Sign Up | </a>
+<a href="pages/login.html"> Login | </a>
+<a href="https://rss.com/"> RSS Feed</a>
+</div>
+      <div class="search-box"> 
+        <left style="padding: 86.4%" ><input type="text" placeholder="   Search..." style="border-radius: 40px">
+        <button type="submit" style="border-radius: 80%"><i class="fas fa-search"></i></button>
+        <!--krijimi i menyse search u 'placeholder' tregon ne background emrin search-->
+        </left>
+    </div> <br>
+    <link rel="stylesheet" type="text/css" href="styless.css">
+
+    </head>
+
+<body style="background-color:black;">
+    
+   
+        <nav>
+     <hr><ul>
+        <li><a href="index.html"><i class="fas fa-home"> Home </i>&nbsp&nbsp&nbsp |</a></li>
+        <li><a href="pages/sports.html">Sports &nbsp&nbsp&nbsp |</a></li>
+        <li><a href="pages/stanlee.html">Lifestyle &nbsp&nbsp&nbsp |</a></li>
+        <li><a href="pages/tech.html">Tech &nbsp&nbsp&nbsp |</a></li>
+        <li><a href="pages/longtextlink.html">Long text link &nbsp&nbsp&nbsp |</a></li>
+        <li><a href="pages/contact.html">Contact Us &nbsp&nbsp&nbsp |</a></li>
+        <li><a href="pages/register.html"> Login or Register </a></li>
+        <li><a href="#">More Options</a>
+            <ul>
+            <li><a href="pages/sistemi%20diellor.html">Sistemi Diellor</a></li><hr>
+        	<li><a href="pages/register.html"> Sign Up </a></li><hr>
+        </ul>
+    </nav><br> <hr>
+       <div id="fotot">
+   
+<center> <h1 style="color:Gray">
+MONUMENTAL - World's largest online news destination.
+<p id="Paragrafi">
+<h4 style="color:Gray"><center>
+<i>"We are renowned for the analysis, perspective, opinion and insight that our journalism provides to a diverse and discerning audience.<br>  Every day the content we create – in print, online, in our apps and across many other platforms – is setting the news agenda, sparking debate and provoking comment. „</i></center></h4></h1></center></div>
+    <br>
+     <div class="f1">
+    <div class="t1">
+        <a href="pages/LM10.html"><p>Luka Modric elected as Best 2018/19 Player</p></a>
+        </div>
+    </div>
+    <div class="f2">
+    <div class="t2">
+        <a href="https://www.independent.co.uk/arts-entertainment/tv/game-of-thrones/game-of-thrones-season-8-release-date-spoilers-plot-cast-watch-trailer-jon-snow-tyrion-cersei-a8655716.html" target="_blank"><p>GOT S8 Release Date Confirmed!</p></a>
+        </div>
+    </div>
+    <div class="f3">
+    <div class="t3">
+        <a href="pages/stanlee.html" target="_blank"><p style="padding-left: 50px;"> Marvel Comics Legend Stan Lee Dead at 95</p></a>
+        </div>
+    </div>
+ <link rel="stylesheet" href="styles.css">
+<div id="tekstet">
+    <div id="tekst1">
+        <pre>
+    <p style="font-size:20px;color: black;"><img src="images/p1.jpg" width="46px" height="46px" alt="!" marging-right="5px">Future Cars
+    <p style="color: black;">Manufacturers will be bringing back models from the
+    dead and adding all-new models to satisfy the industry’s 
+    thirst for new cars.Introducing completely redesigned cars 
+    such Ford Ranger,Chevrolet Silverado 1500 and Tesla X.</p>
+    </pre>
+    </div>
+    <div id="tekst1">
+        <pre>
+    <p style="font-size: 20px;color: black;"><img src="images/p2.jpg" width="46px" height="46px" alt="!">Healthline
+    <p style="color: black;">Protein is a tough nutrient to top in terms of 
+    importance.Protein foods can help you lose weight, feel 
+    better, and even gain muscles.Fish is healthy,high-protein
+    food that has a rightful place in a well-balanced diet.</p>
+    </pre>
+    </div>
+    <div id="tekst1">
+        <pre>
+    <p style="font-size: 20px;color: black;"><img src="images/p3.jpg" width="46px" height="46px" alt="!">Space
+    <p style="color: black;">Billionaire entrepreneurs are trying to create 
+    rockets fit for human travel, while government agencies 
+    spend billions furthering their explorations. But we are
+    still a long way off from making our way to the red planet</p>
+    </pre>
+    </div>
+    <div id="tekst2">
+        <pre>
+    <p style="font-size: 20px;color: black;"><img src="images/pollution.png" width="46px" height="46px" alt="!">Pollution
+    <p style="color: black;">Nothing is more vital to life than breathing:
+    in a lifetime, about 250m litres of air passes through 
+    your lungs. Yet walk along a busy city street and you will 
+    inhale something like 20m particles in a single lungful.</p>
+    </pre>
+    </div>
+    <div id="tekst2">
+        <pre>
+    <p style="font-size: 20px;color: black"><img src="images/internet.jpg" width="46px" height="46px" alt="!">The Internet</p>
+    <p style="color: black;">The Chinese telecoms firm Huawei estimates that the 
+    information and communications technology(ICT) industry 
+    could use 20% of the world’s electricity and release 
+    more than 5% of the world’s carbon emissions by 2025.
+    </p>
+    </pre>
+    </div>
+    <div id="tekst2">
+        <pre>
+    <p style="font-size: 20px;color: black;"><img src="images/nuke.jpg" width="46px" height="46px" alt="!">Nuclear power</p>
+    <p style="color: black;">The use of a nuclear weapon is now more likely than
+    any time since the cold war, but the probability of 
+    humanity being wiped out entirely has diminished
+    Whatever happened to nuclear disarmament?</p>
+    </pre></div>
+    
+    <table style="color: aliceblue;background-color: black;">
+
+    <tr>
+        <th><h2 style="margin-left: -1.7%; color: #cccccc;">From the blog</h2></th>
+        <th><h2 style="margin-left: -100%; color: #cccccc;">Quick Links</h2></th>
+        <th><h2 style="margin-left: -5%; color: #cccccc;">Contact Us</h2></th>
+        <th><h2 style="margin-left: -5%; color: #cccccc;">Socialise</h2></th>
+
+    </tr>
+    <tr><td><p style="font-family: arial;"><b>Luka Modric</b> wins Ballon d'Or,<br>ending decade of Ronaldo and Messi triumphs.<br><br>LM10, the midfield maestro who won<br>the Champions Legue title with Real Madrid<br>and then helped lead Croatia to the World Cup final<br> was honored Monday with the Ballon d'Or as <br>world's best player of the year <br>  
+    <span style="padding-left: 310px;"><a href="pages/longtextlink.html" style="text-align: center;">Read More &raquo;</a></p></span></td>
+
+        
+
+    <td id="linku" ><a href="#Paragrafi" class="active" ><span style="margin-left: -90%;">This link directs to a web-page's ID</a><br><hr style="margin-left: -90%;"></span><br>
+    <a href="https://github.com/" class="active" target="_blank"><span style="margin-left: -90%;">This link directs to an active website on internet</a><br><hr style="margin-left: -90%;"></span><br>
+      <a href="images/p3.jpg" class="active" target="_blank"><span style="margin-left: -90%;">This link directs to a photo taken by PC</a><br><hr style="margin-left: -90%;"></span><br>
+    <a href="mailto:vehbi.res@gmail.com" class="active"><span style="margin-left: -90%;">This link directs to an e-mail address</a><br><hr style="margin-left: -90%;"></span><br>
+        <a href="#Home" class="active"><span style="margin-left: -90%;">This link directs to web-page's header</a> <br><hr style="margin-left: -90%;"></span></td>
+        <td><b>Company:</b> Monumental<br><b>Street:</b> Calle Bailén <b>Str.</b><br><b>Town:</b> Madrid<br><b>ZipCode:</b>28047<br><br><b>Tel:</b><br>049 123/456<br><b>Fax:</b><br>
+        38349 123/456<br><br><b>Email:</b><br>team@monumental.com
+        </td>
+        <td><div class="rrjetet-sociale"><a href="https://www.linkedin.com/" target="_blank"><img src="images/linkedin.png" width="16px" height="16px"> Linkedin</a><hr><br><br>
+            <a href="https://www.twitter.com" target="_blank"><img src="images/twitter1.PNG" width="16px" height="16px"> Twitter</a><hr><br><br>
+            <a href="https://www.pinterest.com/" target="_blank"><img src="images/pinterest1.png" width="16px" height="16px"> Pinterest</a><hr><br><br>
+            <a href="https://www.rss.com/" target="_blank"><img src="images/rss1.png" width="16px" height="16px"> Rss Feed</a><hr><br></div>
+      </td>
+     </tr>
+    </table></div><hr>
+    <p><em> All rights reserved &copy;2019 Monumental TEAM</em></p>
+   </body></html>
