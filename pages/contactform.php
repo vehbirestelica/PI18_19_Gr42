c94cf4f0eb40ce00fe00f5d451b3f8284cad7dd3
Vehbi Restelica <vehbi.res@gmail.com>
2019-05-28 18:06:18 +0200

    Update contactform.php

--- pages/contactform.php
+++ pages/contactform.php
@@ -30,3 +30,65 @@
 		<div class="success"><?= $success; ?></div>
 	</form>
 </div>
+<span><?php
+
+	class Pagination {
+
+	    function __construct() {
+	        print "";
+	    }
+		var $data;
+
+		function Paginate($values, $per_page){
+
+			echo "\n\nYou can Find us on :";
+			$total_values = count($values);
+			
+			if (isset($_GET['page']))
+			{
+				$current_page=$_GET['page'];
+			}
+			else {
+				$current_page=1;
+			}
+			$counts=ceil($total_values/$per_page);
+			$param1=($current_page-1)*$per_page;
+			$this->data=array_slice($values,$param1,$per_page);
+
+			for($x=1;$x<=$counts; $x++)
+			{
+				$numbers[]=$x;
+				// $numbers[0]='Fb, ';
+				// $numbers[1]='YouTube, ';
+				// $numbers[2]='Twitter, ';
+				// $numbers[3]='GitHub.';
+			}
+			return $numbers;
+		}
+		function fetchResult() {
+
+			$resultValues = $this->data;
+			return $resultValues;
+		}
+	}
+	$pag = new Pagination();
+	$data=array("Facebook", "YouTube", "Twitter", "GitHub");
+	$numbers=($pag->Paginate($data,1));
+
+	$result=$pag->fetchResult();
+
+	foreach($result as $r) {
+
+		echo '<div>'.$r.'</div>';
+
+	}
+
+	foreach ($numbers as $num ) {
+		echo '<a href="contactform.php?page='.$num.'">'.$num.'</a>';
+	}
+
+	    function __destruct() {
+	       print " " . __CLASS__ . "\n";
+	    }
+	$obj = new Pagination();
+?></span>
