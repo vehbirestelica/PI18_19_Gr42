b33afe8817ad07ff4bdd58be7b6c2acc75b5bf12
Vehbi Restelica <vehbi.res@gmail.com>
2019-06-01 23:57:52 +0200

    Create index.php

---
+++ pages/game/index.php
@@ -0,0 +1,34 @@
+<?php
+require_once('tictactoe.php');
+session_start();
+?>
+<html>
+<head>
+    <title>Tic-Tac-Toe</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css" />
+</head>
+<body>
+<div id="main">
+    <form action="game.php" method="POST">
+        <h1>Tic Tac Toe</h1>
+        <?php
+        if(isset($_SESSION['game'])){
+            echo unserialize($_SESSION['game']);
+        } else {
+        ?>
+        <div class="table">
+        <h3>Choose which player goes first</h3>
+        <table>
+        <tr><td><input type="radio" name="player" value="X" id="x" />
+        <label for="x"><img src="img/X.jpg" /></label></td>
+        <td><input type="radio" name="player" value="O" id="o" />
+        <label for="o"><img src="img/O.jpg" /></label></td></tr>
+
+        <tr><input type="submit" value="submit" name="submit" /></tr>
+        </table>
+        </div>
+    </form>
+    <?php } ?>
+</div>
+</body>
+</html>
