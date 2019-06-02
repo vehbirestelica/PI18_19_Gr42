046e586e4651301a0d6a068d05332da85a397fed
Vehbi Restelica <vehbi.res@gmail.com>
2019-06-02 23:42:54 +0200

    Update AJAXChatTemplate.php

--- chat/lib/class/AJAXChatTemplate.php
+++ chat/lib/class/AJAXChatTemplate.php
@@ -1,11 +1,4 @@
 <?php
-/*
- * @package AJAX_Chat
- * @author Sebastian Tschan
- * @copyright (c) Sebastian Tschan
- * @license Modified MIT License
- * @link https://blueimp.net/ajax/
- */
 
 // Class to handle HTML templates
 class AJAXChatTemplate {
