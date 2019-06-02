0b948781f8f832e4a3e5820387c6a8c459a05744
Vehbi Restelica <vehbi.res@gmail.com>
2019-06-02 23:43:07 +0200

    Update classes.php

--- chat/lib/classes.php
+++ chat/lib/classes.php
@@ -1,11 +1,4 @@
 <?php
-/*
- * @package AJAX_Chat
- * @author Sebastian Tschan
- * @copyright (c) Sebastian Tschan
- * @license Modified MIT License
- * @link https://blueimp.net/ajax/
- */
 
 // Include Class libraries:
 require(AJAX_CHAT_PATH.'lib/class/AJAXChat.php');
