62ec64a52d0948b4496d99e1944af7049fb68d90
Vehbi Restelica <vehbi.res@gmail.com>
2019-06-02 23:42:21 +0200

    Update AJAXChat.php

--- chat/lib/class/AJAXChat.php
+++ chat/lib/class/AJAXChat.php
@@ -1,11 +1,4 @@
 <?php
-/*
- * @package AJAX_Chat
- * @author Sebastian Tschan
- * @copyright (c) Sebastian Tschan
- * @license Modified MIT License
- * @link https://blueimp.net/ajax/
- */
 
 // Ajax Chat backend logic:
 class AJAXChat {
